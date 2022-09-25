5ed857e3eab7eec9b67e6bc33ba940bf4be9705e
carlossabino12 <carlossabino12@gmail.com>
2022-09-24 22:39:31 -0300

    Configurando o ambiente

---
+++ src/test/java/modulos/produtos/ProdutosTest.java
@@ -0,0 +1,30 @@
+package modulos.produtos;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Teste Web do modulo de Produtos")
+public class ProdutosTest {
+
+    @Test
+    @DisplayName("Não é permitido registrar um produto com valor igual a zero")
+    public void testNaoEPermitidoResgistrarProdutosComValorIgualAZero(){
+
+        // Abrir o navegador
+
+        // Navegar para a página da Lojinha Web
+
+        // Fazer login
+
+        // Vou para a tela de registrar produto
+
+        // Vou preencher as informações do produto e o valor igual a zero
+
+        // Vou submeter o formulário
+
+        // Vou validar se a mesagem de erro foi apresentada
+
+
+    }
+
+}
